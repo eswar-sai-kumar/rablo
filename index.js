af69787f5426a46bacb545bc2dcfadcf91b9fdc0
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-08-20 07:08:02 +0530

    update

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const moment = require('moment-timezone');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  const timestampIST = moment().tz('Asia/Kolkata').format(); // Get current IST time
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+  res.json({
+    message: "Hello World", // Here is the added message
+    timestamp: timestampIST,
+    ip: ip
+  });
 });
 
-const PORT = 8080;
+const PORT = 80;
 app.listen(PORT, () => {
   console.log(`SimpleTimeService running at http://localhost:${PORT}`);
 });
