4d7385e38d471216282d4dc5bb45211f18b84932
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-08-20 06:41:16 +0530

    update

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const moment = require('moment-timezone');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  const timestampIST = moment().tz('Asia/Kolkata').format(); // Get current IST time
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-
-  res.json({
-    timestamp: timestampIST,
-    ip: ip
-  });
+  res.send('Hello World');
 });
 
 const PORT = 8080;
